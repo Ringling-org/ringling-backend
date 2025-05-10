7e74032dbedfb36356e880bf5f9c935ddf7258bf
신성현 <froggy0m0a@gmail.com>
2025-05-10 11:03:28 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/ringling/backend/MainApplication.java
@@ -0,0 +1,10 @@
+package org.ringling.backend;
+
+import froggy.winterframework.boot.WinterApplication;
+
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+        WinterApplication.run(MainApplication.class);
+    }
+}
